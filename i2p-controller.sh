317ca7badc7d5c150f62647a9710e65f1b0a947b
Philip Colllier <redcrown01@gmail.com>
2022-08-07 05:25:19 +0000

    Proxychains and Workaround

--- i2p-controller.sh
+++ i2p-controller.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-# Copyright (c) 2021 by Philip Collier, <webmaster@mofolinux.com>
+# Copyright (c) 2021 by Philip Collier, github.com/AB9IL
 # This script is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version. There is NO warranty; not even for
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
+# The file /tmp/session_proxy is used as a work-around because proxychains
+# is FUBAR when using browsers based on Chrome.
+
 Encoding=UTF-8
 
+# define the web browser
+#startbrowser="forefox"
+startbrowser="vivaldi"
+
 i2pstart(){
+touch /tmp/proxyflag
+sudo sed -i 's/^### FZPROXY.*/### FZPROXY\nsocks5 127.0.0.1 14447/' /etc/proxychains4.conf
+echo 'socks5://127.0.0.1:14447' > /tmp/session_proxy
 export https_proxy=127.0.0.1:4444
 export HTTPS_PROXY=127.0.0.1:4444
 export http_proxy=127.0.0.1:4444
@@ -19,10 +29,15 @@ export SOCKS_PROXY=127.0.0.1:14447
 export NO_PROXY='localhost, 127.0.0.1'
 export no_proxy='localhost, 127.0.0.1'
 /usr/bin/i2prouter start;
+#$startbrowser --proxy-server="socks5://127.0.0.1:14447" \
+#    --new-window "http://127.0.0.1:7657/home"
 }
 
 i2pstop(){
+rm /tmp/proxyflag
 /usr/bin/i2prouter stop;
+sudo sed -i '/^### FZPROXY.*/q' /etc/proxychains4.conf
+> /tmp/session_proxy
 export https_proxy=
 export HTTPS_PROXY=
 export http_proxy=
